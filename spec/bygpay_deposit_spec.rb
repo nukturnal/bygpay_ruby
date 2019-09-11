18405873d79ef55e4bb6a02d128a3d1c6fd35910
Alfred Rowe <alfred.rowe@gmail.com>
2019-09-11 05:05:19 +0000

    Test Specs

--- spec/bygpay_deposit_spec.rb
+++ spec/bygpay_deposit_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Bygpay::Deposits do
       end
 
       it 'should return valid transaction status flag' do
-        expect(['accepted','pending','fail','success']).to include(@mobile_deposit.status)
+        expect(%w(accepted pending fail success)).to include(@mobile_deposit.status)
       end
 
       it 'should return true when valid currency is included in payload' do
@@ -59,6 +59,61 @@ RSpec.describe Bygpay::Deposits do
         expect(@mobile_deposit.response.currency).not_to be_empty
         expect(@mobile_deposit.amount).to be > 0
         expect(@mobile_deposit.response.provider).not_to be_empty
+      end
+    end
+
+    context 'Rave Redirect' do
+      it 'should return true we call the charge method' do
+        response = @rave_redirect.charge(@deposit_amount, @valid_rave_payload)
+        expect(response).to eql true
+      end
+
+      it 'should not have an empty transaction_id when charge method returns true' do
+        # This relies directly on the above spec (for speed)
+        # Change this with caution
+        expect(@rave_redirect.transaction_id).not_to be_empty
+      end
+
+      it 'should return true when transaction_status method is called' do
+        expect(@rave_redirect.transaction_status(@rave_redirect.uuid)).to eql true
+      end
+
+      it 'should return valid transaction status flag' do
+        expect(%w(accepted pending fail success)).to include(@rave_redirect.status)
+      end
+
+      it 'should return true when valid currency is included in payload' do
+        response = @rave_redirect.charge(@deposit_amount, @valid_rave_payload.merge(@valid_currency))
+        expect(response).to eql true
+      end
+
+      it 'should return false when invalid currency is included in payload' do
+        response = @rave_redirect.charge(@deposit_amount, @valid_rave_payload.merge(@invalid_currency))
+        expect(response).to eql false
+      end
+
+      it 'response_text should not be empty on validation error for charge method call' do
+        expect(@rave_redirect.response_text).not_to be_empty
+      end
+
+      it 'status after charge method validation error should return fail' do
+        expect(@rave_redirect.status).to eql 'fail'
+      end
+
+      it 'response variable of the deposit object should be an instance of Bygpay::BygResponse' do
+        expect(@rave_redirect.response).to be_instance_of Bygpay::BygResponse
+      end
+
+      it 'Bygpay::BygResponse methods should return expected results' do
+        @rave_redirect.charge(@deposit_amount, @valid_rave_payload)
+
+        expect([true, false]).to include(@rave_redirect.response.success?)
+        expect([true, false]).to include(@rave_redirect.response.failed?)
+        expect([true, false]).to include(@rave_redirect.response.pending?)
+        expect(@rave_redirect.response.processor_transaction_id).to be_nil
+        expect(@rave_redirect.response.redirect_link).not_to be_empty
+        expect(@rave_redirect.response.currency).not_to be_empty
+        expect(@rave_redirect.amount).to be > 0
       end
     end
 

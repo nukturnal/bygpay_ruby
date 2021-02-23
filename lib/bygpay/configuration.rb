868600f2c1e6e14213123b0dce5668390eb7491b
Alfred Rowe <alfred@encodevlabs.com>
2021-02-23 17:57:40 +0000

    Add deposit_paystack_path accessor

--- lib/bygpay/configuration.rb
+++ lib/bygpay/configuration.rb
@@ -3,7 +3,7 @@ module Bygpay
     attr_accessor :base_url, :api_key,
                   :deposit_mobile_path, :deposit_status_overide_path, :deposit_status_path,
                   :withdraw_mobile_path, :withdraw_status_overide_path, :withdraw_status_path,
-                  :deposit_card_path, :deposit_rave_path, :deposit_expresspay_path
+                  :deposit_card_path, :deposit_rave_path, :deposit_expresspay_path, :deposit_paystack_path
 
     def initialize
       @base_url = nil
